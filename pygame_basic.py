538c140212fe53bffa44274509e2ab053ea72d1a
acro.t_watanabe <t_watanabe@acroquest.co.jp>
2020-08-27 00:38:00 +0900

    サンプルひな形プログラム追加

---
+++ pygame_basic.py
@@ -0,0 +1,26 @@
+import pygame
+from pygame.locals import *
+
+def main():
+
+    '''初期設定'''
+
+    '''登場する人/物/背景の作成'''
+       
+    while True:
+        
+        '''画面(screen)をクリア'''
+
+        '''ゲームに登場する人/物/背景の位置Update'''
+            
+        '''画面(screen)上に登場する人/物/背景を描画'''
+        
+        '''画面(screen)の実表示'''
+
+        '''イベント処理'''
+
+        '''描画スピードの調整（FPS)'''
+
+if __name__ == "__main__":
+    main()
+        
